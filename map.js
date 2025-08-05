701957007424e3f3fbbf1dc086bc63a7768ce334
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2025-08-05 13:39:55 +0200

    Added S shelf

--- map.js
+++ map.js
@@ -17,7 +17,7 @@ function inventoryShelfToShelfId(shelfIdString, shelves) {
     const shelfIds = shelves.map(s => s.shelfId);
 
     // Check our special shelves
-    if (["C", "D", "G"].includes(firstLetter)) {
+    if (["C", "D", "G", "S"].includes(firstLetter)) {
         debugger;
         for (const id of shelfIds) {
             if (firstLetter === id[0]) {
