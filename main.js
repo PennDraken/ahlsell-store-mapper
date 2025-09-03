429a3f5ef91d1af58d76573d30953d5e86d1467d
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2025-09-03 12:08:55 +0200

    Added images to articles maybe

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let currentFilter = "";
     let debounceTimeout = null;
+
+    function getImageURL(article_number) {
+        // Example URL calls. Note many articles will not show up using this method.
+        // https://www.ahlsell.se/external-assets/JPEGlarge800_800/25/16/ProductImage75172516.jpg?preset=medium
+        // https://www.ahlsell.se/external-assets/JPEGlarge800_800/_5/66/7201_1_566.jpg?preset=medium
+        const lastDigitsPart1 = article_number.substring(article_number.length - 4, article_number.length - 2)
+        const lastDigitsPart2 = article_number.substring(article_number.length - 2, article_number.length)
+        return "https://www.ahlsell.se/external-assets/JPEGlarge800_800/" + lastDigitsPart1 + "/" + lastDigitsPart2 + "/ProductImage" + article_number + ".jpg?preset=medium"
+    }
+    
     function renderInventory(filter = "") {
         const normalized = filter.toLowerCase();
 
@@ -21,8 +31,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
             for (const item of results) {
                 const row = document.createElement("tr");
+                const imageURL = getImageURL(article_number);
                 row.innerHTML = `
                     <td id="location-cell">${item.location}</td>
+                    <td><img src="${imageURL}" alt="" border=3 height=30></img></th>
                     <td id="name-cell">${item.name1}</td>
                     <td>${item.name2}</td>
                     <td>${item.article_number}</td>
